e052601759e37053b88b6601bef2f55c6096ae06
cj <cj@zv-lab.com>
2024-03-05 17:47:09 +0900

    Added a lib comment code

--- FZLib/Helpers/StringHelper.h
+++ FZLib/Helpers/StringHelper.h
@@ -3,9 +3,14 @@
 #define __FZ_LIBRARY_STRING_HELPER__
 
 #ifdef FZLIB_EXPORTS
-#define FZLIB_API __declspec(dllexport)
+	#define FZLIB_API __declspec(dllexport)
 #else
-#define FZLIB_API __declspec(dllimport)
+	#define FZLIB_API __declspec(dllimport)
+		#ifdef __DEBUG
+			#pragma comment (lib, "../Dist/lib/Debug/x86_64/FZLib.lib")
+		#else
+			#pragma comment (lib, "../Dist/lib/Release/x86_64/FZLib.lib")
+		#endif
 #endif
 
 extern "C" {
