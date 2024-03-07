aa9f210f6df2372fda9d2db6bcb9077a326d7ab9
cj <cj@zv-lab.com>
2024-03-07 14:34:40 +0900

    Log Format 부분의 유틸성 개선 및 렌더러 기능 추가 준비

---
+++ FZLib/Renderer/Renderer.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef __FZ_LIBRARY_RENDERER__
+#define __FZ_LIBRARY_RENDERER__
+
+#ifdef FZLIB_EXPORTS
+#define FZLIB_API __declspec(dllexport)
+#else
+#define FZLIB_API __declspec(dllimport)
+#endif
+
+
+
+#endif
